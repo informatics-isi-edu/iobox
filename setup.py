8640ca80ff2c61e81d48bc368f248a5d803fe5f7
robes <robes@users.noreply.github.com>
2013-12-11 03:32:44 +0000

    Initial skeleton of the outbox

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(name='ermrest-iobox',
       description='ERMREST IOBox',
       packages=['iobox'],
       package_dir={'': 'src'},
-      #package_data={'iobox': ['sql/*.sql']},
+      #package_data={'iobox': ['data/*']},
       scripts=['bin/ermrest-outbox']
       )
