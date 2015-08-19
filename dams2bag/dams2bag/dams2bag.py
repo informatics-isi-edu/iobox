2e779517a9f8eb2115bf17ce768a449ae8a38455
mikedarcy <mikedarcy@users.noreply.github.com>
2015-08-19 12:47:10 -0700

    Support creating fetch.txt from a query result as an output mode. Support "prefetching" files into the bag from a query result as an output mode. Both modes require the query output to be formatted as CSV with the following columns: URL, LENGTH, FILENAME as specified in the bagit spec for fetch.txt tag files. Various cleanup.

--- dams2bag/dams2bag/dams2bag.py
+++ dams2bag/dams2bag/dams2bag.py
@@ -4,6 +4,7 @@ import shutil
 import os.path
 import urlparse
 import requests
+import csv
 import bagit
 import simplejson as json
 
@@ -30,7 +31,7 @@ def open_session(host, user_data):
 
     r = requests.post(url, verify=False, data=user_data)
     if r.status_code > 203:
-        sys.stdout.write('Open Session Failed with Status Code: %s %s\n' % (r.status_code, r.text))
+        print 'Open Session Failed with Status Code: %s %s\n' % (r.status_code, r.text)
         sys.exit(1)
 
     c = cookielib.Cookie(version=0,
@@ -55,24 +56,23 @@ def open_session(host, user_data):
     return cj
 
 
-def get_file(query_url, output_path, headers, cookie_jar):
+def get_file(url, output_path, headers, cookie_jar):
     if output_path:
         try:
-            r = requests.get(query_url, headers=headers, stream=True, verify=False, cookies=cookie_jar)
+            r = requests.get(url, headers=headers, stream=True, verify=False, cookies=cookie_jar)
             if r.status_code != 200:
-                sys.stdout.write('Query Failed for url: %s\n' % query_url)
-                sys.stdout.write('Transfer Failed for file [%s]. Status code: %s %s \n\n' %
-                                 (output_path, r.status_code, r.text))
+                print 'GET Failed for url: %s' % url
+                print 'File [%s] transfer failed. Status code: %s %s ' % (output_path, r.status_code, r.text)
                 sys.exit(1)
             else:
-                sys.stdout.write('File [%s] transfer successful. Status code: %s \n' % (output_path, r.status_code))
                 data_file = open(output_path, 'wb')
                 for chunk in r.iter_content(CHUNK_SIZE):
                     data_file.write(chunk)
                 data_file.flush()
                 data_file.close()
+                print 'File [%s] transfer successful. Status code: %s' % (output_path, r.status_code)
         except requests.exceptions.RequestException as e:
-            sys.stdout.write('HTTP Request Exception: %s %s \n' % (e.errno, e.message))
+            print 'HTTP Request Exception: %s %s' % (e.errno, e.message)
 
 
 def create_bag(config):
@@ -86,10 +86,11 @@ def create_bag(config):
     print "Creating bag: %s" % bag_path
 
     if os.path.exists(bag_path):
-        print "Specified bag directory [%s] already exists....deleting it...." % bag_path
+        print "Specified bag directory [%s] already exists -- it will be deleted" % bag_path
         shutil.rmtree(bag_path)
 
     os.makedirs(bag_path)
+    bag = bagit.make_bag(bag_path, {'Contact-Name': contact})
 
     if username and password:
         cookie_jar = open_session(host, {'username': username, 'password': password})
@@ -97,21 +98,70 @@ def create_bag(config):
         cookie_jar = None
 
     for query in config['QUERIES']:
-        query_url = ''.join([host, base_path, query['query_path']])
-        output_file = query['output_file']
-        output_format = query.get('output_format', 'csv')
-        headers = {'accept': 'text/csv'}
-        if output_format == 'json':
+        url = ''.join([host, base_path, query['query_path']])
+        output_name = query['output_name']
+        output_format = query['output_format']
+        if output_format == 'csv':
+            headers = {'accept': 'text/csv'}
+            output_name = ''.join([output_name, '.csv'])
+            output_path = os.path.abspath(''.join([bag_path, os.path.sep, 'data', os.path.sep, output_name]))
+        elif output_format == 'json':
             headers = {'accept': 'application/json'}
-        output_path = os.path.abspath(''.join([bag_path, os.path.sep, output_file, '.', output_format]))
+            output_name = ''.join([output_name, '.json'])
+            output_path = os.path.abspath(''.join([bag_path,  os.path.sep, 'data', os.path.sep, output_name]))
+        elif output_format == 'prefetch':
+            headers = {'accept': 'text/csv'}
+            output_path = os.path.abspath(''.join([bag_path, os.path.sep, 'prefetch.txt']))
+        elif output_format == 'fetch':
+            headers = {'accept': 'text/csv'}
+            output_path = os.path.abspath(''.join([bag_path, os.path.sep, 'fetch.txt']))
+        else:
+            print "Unsupported output type: %s" % output_format
 
-        get_file(query_url, output_path, headers, cookie_jar)
+        get_file(url, output_path, headers, cookie_jar)
 
-    bag = bagit.make_bag(bag_path, {'Contact-Name': contact})
+        if output_format == 'prefetch':
+            print "Prefetching file(s)..."
+            with open(output_path, 'rb') as csv_in:
+                reader = csv.DictReader(csv_in)
+                for row in reader:
+                    prefetch_url = row['URL']
+                    prefetch_length = int(row['LENGTH'])
+                    prefetch_filename = \
+                        os.path.abspath(''.join(
+                            [bag_path, os.path.sep, 'data', os.path.sep, output_name, os.path.sep, row['FILENAME']]))
+                    print "Prefetching %s as %s" % (prefetch_url, prefetch_filename)
+                    os.makedirs(os.path.dirname(prefetch_filename))
+                    get_file(prefetch_url, prefetch_filename, headers, cookie_jar)
+                    file_bytes = os.path.getsize(prefetch_filename)
+                    if prefetch_length != file_bytes:
+                        print "File size of %s does not match expected size of %s for file %s" % \
+                              (prefetch_length, file_bytes, prefetch_filename)
+                        sys.exit(1)
+                csv_in.close()
+                os.remove(output_path)
+
+        elif output_format == 'fetch':
+            print "Writing fetch.txt..."
+            new_csv_file = ''.join([output_path, '.tmp'])
+            with open(output_path, 'rb') as csv_in, open(new_csv_file, 'wb') as csv_out:
+                reader = csv.DictReader(csv_in)
+                writer = csv.DictWriter(csv_out, reader.fieldnames, delimiter='\t')
+                writer.writeheader()
+                for row in reader:
+                    row['FILENAME'] = ''.join([output_name, '/', row['FILENAME']])
+                    writer.writerow(row)
+                csv_in.close()
+                csv_out.close()
+                os.remove(output_path)
+                os.rename(new_csv_file, output_path)
 
     try:
+        print "Updating bag manifests..."
+        bag.save(manifests=True)
+        print "Validating bag..."
         bag.validate()
-        sys.stdout.write('Created valid data bag: %s \n' % bag_path)
+        print 'Created valid data bag: %s' % bag_path
 
     except bagit.BagValidationError as e:
         print "BagValidationError:", e
@@ -124,7 +174,7 @@ def create_bag(config):
 
     try:
         archive = shutil.make_archive(bag_path, 'zip', bag_path)
-        sys.stdout.write('Created valid data bag archive: %s \n' % archive)
+        print 'Created valid data bag archive: %s' % archive
     except:
         print 'Unexpected error while creating data bag archive: ', sys.exc_info()[0]
         raise
