045d6a34a7a7686fc8644c98f99b80266e1a38b3
robes <robes@users.noreply.github.com>
2015-09-04 15:36:18 -0700

    remove deprecated utility

--- deprecated/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-setup(name='ermrest-iobox',
-      version='0.0',
-      description='ERMREST IOBox',
-      packages=['iobox'],
-      package_dir={'': 'src'},
-      package_data={'iobox': ['sql/*.sql']},
-      scripts=['bin/ermrest-outbox', 'bin/ermrest-outbox-oneshot']
-      )
